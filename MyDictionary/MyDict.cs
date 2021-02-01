d182aa0a41023c930e32ed20aed59710cc617836
Sema Dursun <dursun.semaa@gmail.com>
2021-02-01 21:53:53 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDict.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDict<S, A>
+    {
+        S[] arrayKey;
+        S[] tempKey;
+        A[] arrayValue;
+        A[] tempValue;
+
+        public MyDict()
+        {
+            arrayKey = new S[0];
+            arrayValue = new A[0];
+
+        }
+        public void Add(S key, A value)
+        {
+            tempKey = arrayKey;
+            tempValue = arrayValue;
+            arrayKey = new S[arrayKey.Length + 1];
+            arrayValue = new A[arrayValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                arrayKey[i] = tempKey[i];
+            }
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                arrayValue[i] = tempValue[i];
+
+            }
+            arrayKey[arrayKey.Length - 1] = key;
+            arrayValue[arrayValue.Length - 1] = value;
+
+            for (int i = 0; i < arrayKey.Length; i++)
+
+            {
+                Console.WriteLine("Stock:" + arrayKey[i] + "Kitap Adı:" + arrayValue[i]);
+
+            }
+            Console.WriteLine(".....................");
+        
+        }
+
+
+
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<int, string> kitaplar = new MyDict<int, string>();
+            kitaplar.Add(1, "Roman");
+            kitaplar.Add(2, "Bilim-Kurgu");
+            kitaplar.Add(3, "Şiir");
+            kitaplar.Add(4, "Anı");
+            kitaplar.Add(5, "Öykü");
+
+
+}
